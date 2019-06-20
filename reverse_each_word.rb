42b8ccab557408bd4ba6c2f88f5e773c4f4d02c3
Katana Tran <tkatyk21@gmail.com>
2019-06-20 05:14:12 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,11 +1,4 @@
-def reverse_each_word(string)
-  array = string.split
-  arr_new = []
-  array.each do |counter|
-    arr_new << counter.reverse!
-  end
-  return arr_new.join
-end
+
 
 def reverse_each_word(string)
   array = string.split
